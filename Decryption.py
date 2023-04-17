4bda25345f717097a304e2e5303d7b9d0faf4725
unknown <rv8828120@gmail.com>
2023-04-17 22:46:36 +0800

    Decrypt a message again

--- Decryption.py
+++ Decryption.py
@@ -47,3 +47,50 @@ for encrypted in encrypted_message:
 #Print the Outcome
 print(f"\nEncrypted Message: " + encrypted_message)
 print("Decrypted Message: " + decrypted_message)
+
+#Ask the user if they want to decrypt a message once again
+while True:
+    print(f"\nDo you want to decrypt other nessage, again?")
+    yes_or_no = input("Type YES if you want to continue and NO if you want to exit.")
+    if yes_or_no == "YES":
+    
+    #Ask the user to input the encrypted message
+        encrypted_message = input(f"\nType or input your encrypted message: ")
+        decrypted_message = ""
+
+#Count the characters you entered
+        characters_counter = len(encrypted_message)
+        print(f"\nThe count of the characters you have entered is", characters_counter)
+
+#Its time to turn the encrypted message to decrypted message
+        for encrypted in encrypted_message:
+
+#If this symbol "*" is/are in the message, change it to letter a
+            if "*" in encrypted:
+                decrypted_message += "a"
+
+#If this symbol "&" is/are in the message, change it to letter e
+            elif "&" in encrypted:
+                decrypted_message += "e"
+
+#If this symbol "#" is/are in the message, change it to letter i
+            elif "#" in encrypted:
+                decrypted_message += "i"
+
+#If this symbol "+" is/are in the message, change it to letter o
+            elif "+" in encrypted:
+                decrypted_message += "o"
+
+#If this symbol "!" is/are in the message, change it to letter u
+            elif "!" in encrypted:
+                decrypted_message += "u"
+            else:
+                decrypted_message += encrypted
+
+#Print the Outcome
+        print(f"\nEncrypted Message: " + encrypted_message)
+        print("Decrypted Message: " + decrypted_message)
+
+    if yes_or_no == "NO":
+        print("Codeperman is leaving.....")
+        break
