eefefa5e301b31e772d0f86fd2519e7295fd63f1
unknown <rv8828120@gmail.com>
2023-04-17 22:05:33 +0800

    Pseudocode

---
+++ Decryption.py
@@ -0,0 +1,14 @@
+#Vargas, Ruben A.
+#BSCPE 1-4
+#Object-Oriented Programming
+
+#Ask what is the name of the user and greetings
+#Ask the user to input the encrypted message
+#Count the characters you entered
+#Its time to turn the encrypted message to decrypted message
+#If this symbol "*" is/are in the message, change it to letter a
+#If this symbol "&" is/are in the message, change it to letter e
+#If this symbol "#" is/are in the message, change it to letter i
+#If this symbol "+" is/are in the message, change it to letter o
+#If this symbol "!" is/are in the message, change it to letter u
+#Print the Outcome
